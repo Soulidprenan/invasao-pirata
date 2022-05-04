4675543a11b53084c3c023bf4112dbcb5ecc335b
soulidprenan <re1234nanre@gmail.com>
2022-05-03 21:07:45 -0300

    adicionei trajetoria

--- js/CannonBall.js
+++ js/CannonBall.js
@@ -9,6 +9,7 @@ class CannonBall {
     };
     this.ball = Bodies.circle(this.x, this.y, this.radius, options);
     World.add(world, this.ball);
+    this.trajetoria = [];
   }
   display() {
     push();
@@ -21,12 +22,25 @@ class CannonBall {
       this.radius
     );
     pop();
+    if (
+      this.ball.position.x > 0 &&
+      this.ball.position.x > 300 &&
+      this.ball.position.y < 565
+    ) {
+      this.trajetoria.push([this.ball.position.x, this.ball.position.y]);
+    }
+    for (var i = 0; i < this.trajetoria.length; i = i + 1) {
+      image(this.image, this.trajetoria[i][0], this.trajetoria[i][1], 5, 5);
+    }
   }
   shoot() {
-    var newAngle = (cannon.angle - 28) * (3.14/180);
+    var newAngle = (cannon.angle - 28) * (3.14 / 180);
     var velocity = p5.Vector.fromAngle(newAngle);
     velocity.mult(0.3);
     Body.setStatic(this.ball, false);
-    Body.setVelocity(this.ball, {x: velocity.x * (180/3.14), y:velocity.y * (180/3.14)});
+    Body.setVelocity(this.ball, {
+      x: velocity.x * (180 / 3.14),
+      y: velocity.y * (180 / 3.14),
+    });
   }
 }
--- sketch.js
+++ sketch.js
@@ -4,11 +4,15 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Constraint = Matter.Constraint;
 var engine, world, backgroundImg;
-var cannonBase,cannonImg;
+var cannonBase, cannonImg;
 var cannonBall;
 var cannonBallGroup = [];
 
-var canvas, angle = 20, tower, ground, cannon;
+var canvas,
+  angle = 20,
+  tower,
+  ground,
+  cannon;
 
 function preload() {
   backgroundImg = loadImage("./assets/background.gif");
@@ -18,14 +22,13 @@ function preload() {
 }
 
 function setup() {
-
   canvas = createCanvas(1200, 600);
   engine = Engine.create();
   world = engine.world;
-  
+
   var options = {
-    isStatic: true
-  }
+    isStatic: true,
+  };
 
   ground = Bodies.rectangle(0, height - 1, width * 2, 1, options);
   World.add(world, ground);
@@ -33,60 +36,44 @@ function setup() {
   tower = Bodies.rectangle(160, 350, 160, 310, options);
   World.add(world, tower);
 
-  cannon = new Cannon(180,110,160,130,angle,cannonImg,cannonBase); 
-  
+  cannon = new Cannon(180, 110, 160, 130, angle, cannonImg, cannonBase);
 
   angleMode(DEGREES);
 }
 
 function draw() {
-  image(backgroundImg,0,0,1200,600)
+  image(backgroundImg, 0, 0, 1200, 600);
   Engine.update(engine);
 
-  
   rect(ground.position.x, ground.position.y, width * 2, 1);
-  
 
   push();
   imageMode(CENTER);
-  image(towerImage,tower.position.x, tower.position.y, 160, 310);
-  pop();  
+  image(towerImage, tower.position.x, tower.position.y, 160, 310);
+  pop();
 
   cannon.display();
   //cannonBall.display();
-  for (var i = 0; i < cannonBallGroup.length; i = i + 1){
-    if (cannonBallGroup[i]){
+  for (var i = 0; i < cannonBallGroup.length; i = i + 1) {
+    if (cannonBallGroup[i]) {
       cannonBallGroup[i].display();
-
-
     }
-
-
   }
 }
 
-
 function keyReleased() {
-  if(keyCode == RIGHT_ARROW) {
- cannonBallGroup[cannonBallGroup.length - 1].shoot();
+  if (keyCode == RIGHT_ARROW) {
+    cannonBallGroup[cannonBallGroup.length - 1].shoot();
   }
 
   // "2" == 2
   // "2" === 2
 }
 
-function keyPressed(){
-if(keyCode == RIGHT_ARROW) {
-  cannonBall = new CannonBall(cannon.x,cannon.y,30);
-cannonBallGroup.push(cannonBall);
-console.log(cannonBallGroup);
-
-
-}
-
-
-
-
-
-
+function keyPressed() {
+  if (keyCode == RIGHT_ARROW) {
+    cannonBall = new CannonBall(cannon.x, cannon.y, 30);
+    cannonBallGroup.push(cannonBall);
+    console.log(cannonBallGroup);
+  }
 }
